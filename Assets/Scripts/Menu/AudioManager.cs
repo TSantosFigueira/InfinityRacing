59dd1b5909b9c179572a23fcc03e16123534cfb8
Thiago Figueira <tsantosfigueira@outlook.com>
2017-05-14 20:35:12 -0400

    version 0.6.0
    Added background music manager: the user can now disable or enable it
    conviniently, the game will also remeber the last state through
    PlayerPrefs.

---
+++ Assets/Scripts/Menu/AudioManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//! This class manages the background audio sytem, enabling or disabling it
+public class AudioManager : MonoBehaviour {
+
+    public Sprite musicOn;
+    public Sprite musicOff;
+    private Button soundButton;
+    private AudioSource source;
+
+    private void Start()
+    {
+        if(GameObject.FindGameObjectWithTag("soundButton"))
+            soundButton = GameObject.FindGameObjectWithTag("soundButton").GetComponent<Button>();
+
+        source = GameObject.FindGameObjectWithTag("musicPlayer").GetComponent<AudioSource>();
+
+        source.Play();
+        if (PlayerPrefs.GetString("sound") == "on")
+        {
+            ChangeButtonSprite(true);
+            PlayerPrefs.SetString("sound", "on");
+        }
+        else
+        {
+            source.Pause();
+            ChangeButtonSprite(false);
+            PlayerPrefs.SetString("sound", "off");
+        }           
+    }
+
+    public void ToggleSound()
+    {
+        if (source.isPlaying)
+        {
+            source.Pause();
+            ChangeButtonSprite(false);
+            PlayerPrefs.SetString("sound", "off");
+        }
+        else
+        {
+            ChangeButtonSprite(true);
+            PlayerPrefs.SetString("sound", "on");
+
+            if (!source.isPlaying)
+                source.Play();
+            else
+                source.UnPause();
+        }      
+    }
+
+    private void ChangeButtonSprite(bool isMusicPlaying)
+    {
+        if (soundButton)
+        {
+            if (isMusicPlaying)
+                soundButton.image.sprite = musicOn;
+            else
+                soundButton.image.sprite = musicOff;
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//! Responsible for loading new scenes with proper transition
+public class SceneLoader : MonoBehaviour
+{
+    public string scene;
+    public GameObject transition;
+
+    public void Load()
+    {
+        GameObject.FindGameObjectWithTag("soundManager").GetComponent<Sounds>().playSound("button", 0.5f);
+        transition.SetActive(true);
+        StartCoroutine("LoadSceneAsync");
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        yield return new WaitForSeconds(0.9f);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(scene);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
